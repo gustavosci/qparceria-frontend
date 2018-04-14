f6b3e431f153c8e70a075b4a7653b6f6b93d287e
Gustavo Santos <gustavosci@live.com>
2018-04-14 15:37:38 -0300

    Buscando username do token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,9 +4,12 @@ import { HttpClient } from '@angular/common/http';
 import { LocalUser } from '../model/local-user';
 import { StorageService } from './storage.service';
 import { API_CONFIG } from '../config/api.config';
+import { JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
+
+  jwtHelper: JwtHelper = new JwtHelper();
 
   constructor(public http: HttpClient, public storage: StorageService) {
   }
@@ -24,7 +27,8 @@ export class AuthService {
   successfulLogin(authorizationValue: string){
     let tok = authorizationValue.substring(7); // Retira Bearer do início do token
     let user : LocalUser = {
-      token: tok 
+      token: tok,
+      username: this.jwtHelper.decodeToken(tok).sub
     };
     this.storage.setLocalUser(user);
   }
